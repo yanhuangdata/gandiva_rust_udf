913d4abdffbec8919c2c9c13cfc1fb30909eea37
Yue Ni <niyue.com@gmail.com>
2023-06-07 15:45:21 +0800

    Add is_ascii function.

---
+++ is_ascii_func/src/lib.rs
@@ -0,0 +1,17 @@
+use gandiva_rust_udf_macro::udf;
+
+#[udf]
+pub fn is_ascii(data: &str) -> bool {
+    data.is_ascii()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_is_ascii() {
+        assert!(is_ascii("hello"));
+        assert!(!is_ascii("你好"));
+    }
+}
