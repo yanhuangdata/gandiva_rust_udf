6a72494d5fdacf8fcac7b93b99c0ec2f6583e918
riki <zhaojiaheng@yanhuangdata.com>
2025-02-17 10:54:36 +0800

    fix base ip

--- cidr_func/src/lib.rs
+++ cidr_func/src/lib.rs
@@ -14,7 +14,11 @@ pub fn netmask(ip: &str) -> Result<String, String> {
 #[udf]
 pub fn base_ip(ip: &str) -> Result<String, String> {
     match ip.parse::<IpNetwork>() {
+<<<<<<< Updated upstream
         Ok(network) => Ok(network.ip().to_string()),
+=======
+        Ok(network) => Ok(network.network().to_string()),
+>>>>>>> Stashed changes
         Err(_) => Ok("Invalid cidr specification".to_string())
     }
 }
@@ -152,6 +156,17 @@ mod tests {
     }
 
     #[test]
+<<<<<<< Updated upstream
+=======
+    fn base_ip_ipv4_works1() {
+        let result = base_ip("192.168.1.100/24");
+        assert!(result.is_ok());
+        let value = result.unwrap();
+        assert_eq!(value, "192.168.1.0");
+    }
+
+    #[test]
+>>>>>>> Stashed changes
     fn base_ip_ipv6_works() {
         let result = base_ip("2001:db8::/32");
         assert!(result.is_ok());
@@ -319,11 +334,29 @@ mod tests {
     }
 
     #[test]
+<<<<<<< Updated upstream
     fn base_ip_ipv4_1_works() {
         let result = base_ip("10.88.135.144/0");
         assert!(result.is_ok());
         let value = result.unwrap();
         assert_eq!(value, "10.88.135.144");
+=======
+    fn base_ip_ipv4_test_cases() {
+        let result = base_ip("192.168.1.100/24");
+        assert!(result.is_ok());
+        let value = result.unwrap();
+        assert_eq!(value, "192.168.1.0");
+
+        let result = base_ip("10.10.10.10/8");
+        assert!(result.is_ok());
+        let value = result.unwrap();
+        assert_eq!(value, "10.0.0.0");
+
+        let result = base_ip("172.16.100.100/16");
+        assert!(result.is_ok());
+        let value = result.unwrap();
+        assert_eq!(value, "172.16.0.0");
+>>>>>>> Stashed changes
     }
 
     #[test]
@@ -372,7 +405,11 @@ mod tests {
         let result = base_ip("2001:db8::/0");
         assert!(result.is_ok());
         let value = result.unwrap();
+<<<<<<< Updated upstream
         assert_eq!(value, "2001:db8::");
+=======
+        assert_eq!(value, "::");
+>>>>>>> Stashed changes
     }
 
     #[test]
